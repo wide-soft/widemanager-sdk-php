e195ad556fc50ad264c36651904f9f02797e5bfb
Claudio Almeida <claudioalmeiida@gmail.com>
2017-12-06 20:09:13 -0300

    Start

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# widemanager-sdk-php
-Biblioteca de integração em PHP 
+# Biblioteca de integração em PHP
+
+[![Packagist](https://img.shields.io/packagist/v/wide-soft/widemanager-sdk-php.svg)](https://packagist.org/packages/wide-soft/widemanager-sdk-php)
+
+Para instalar a biblioteca PHP, basta baixá-la <a href="https://github.com/wide-soft/widemanager-sdk-php/archive/master.zip" target="_blank">aqui</a> e copiá-la para a pasta do seu projeto. Você também pode instalá-la usando o <a href="https://getcomposer.org" target="_blank">Composer</a> via <a href="https://packagist.org/packages/wide-soft/widemanager-sdk-php" target="_blank">Packagist</a>.
+
+```
+$ composer require wide-soft/widemanager-sdk-php
+```
+
+Inclusão via arquivo composer.json
+
+```json
+{
+    "require": {
+       "wide-soft/widemanager-sdk-php": "1.*"
+    }
+}
+```
---
+++ src/WideManager.php
@@ -0,0 +1,61 @@
+<?php
+
+class WideManager {
+
+	private $dominio = null;
+	private $autenticacao = array();
+
+	public $requisicoes = array();
+
+	public function __construct($dominio, $email, $token) {
+
+		$this->dominio = $dominio;
+		$this->autenticacao = array(
+			'email' => $email,
+			'token' => $token
+		);
+
+	}
+
+	public function api($local, $parametros = array()) {
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, 'http://' . $this->dominio . '/' . trim($local, '/'));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json')); 
+		curl_setopt($curl, CURLOPT_USERPWD, $this->autenticacao['email'] . ':' . $this->autenticacao['token']);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parametros));
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+		$exec = curl_exec($curl);
+		curl_close($curl);
+
+		if ($exec) {
+
+			$requisicao = json_decode($exec, true);
+
+			if (!is_array($requisicao)) {
+
+				$requisicao = array(
+					'success' => false,
+					'error' => 'Não foi possível tratar o retorno.'
+				);
+
+			}
+
+		} else {
+
+			$requisicao = array(
+				'success' => false,
+				'error' => 'Sem comunicação com o servidor.'
+			);
+
+		}
+
+		$this->requisicoes[] = (object) $requisicao;
+
+		return end($this->requisicoes);
+
+	}
+
+}
