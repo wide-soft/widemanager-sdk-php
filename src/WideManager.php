cc23e2a8dd2ffd7a7d8a35438524fdc9ad33dd72
Claudio Almeida <claudioalmeiida@gmail.com>
2018-08-16 23:06:25 -0400

    Update SDK

--- src/WideManager.php
+++ src/WideManager.php
@@ -19,36 +19,54 @@ class WideManager {
 
 	public function api($local, $parametros = array()) {
 
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, 'http://' . $this->dominio . '/api/' . trim($local, '/'));
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json')); 
-		curl_setopt($curl, CURLOPT_USERPWD, $this->autenticacao['email'] . ':' . $this->autenticacao['token']);
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parametros));
-		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-		$exec = curl_exec($curl);
-		curl_close($curl);
-
-		if ($exec) {
-
-			$requisicao = json_decode($exec, true);
-
-			if (!is_array($requisicao)) {
-
-				$requisicao = array(
-					'success' => false,
-					'error' => 'Não foi possível tratar o retorno.'
-				);
-
-			}
-
-		} else {
+		if (!$this->dominio) {
 
 			$requisicao = array(
 				'success' => false,
-				'error' => 'Sem comunicação com o servidor.'
+				'error' => 'É necessário informar o domínio para efetuar a comunicação.'
 			);
+
+		} else if (!$this->autenticacao['email'] || !$this->autenticacao['token']) {
+
+			$requisicao = array(
+				'success' => false,
+				'error' => 'É necessário informar o e-mail e o token para efetuar a autenticação.'
+			);
+
+		} else {
+
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, 'http://' . $this->dominio . '/api/' . trim($local, '/'));
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json')); 
+			curl_setopt($curl, CURLOPT_USERPWD, $this->autenticacao['email'] . ':' . $this->autenticacao['token']);
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parametros));
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+			$exec = curl_exec($curl);
+			curl_close($curl);
+
+			if ($exec) {
+
+				$requisicao = json_decode($exec, true);
+
+				if (!is_array($requisicao)) {
+
+					$requisicao = array(
+						'success' => false,
+						'error' => 'Não foi possível tratar o retorno.'
+					);
+
+				}
+
+			} else {
+
+				$requisicao = array(
+					'success' => false,
+					'error' => 'Sem comunicação com o servidor.'
+				);
+
+			}
 
 		}
 
