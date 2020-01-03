e48987e41c894f90c42b3971c7b949ded8cf1e29
Claudio Almeida <claudioalmeiida@gmail.com>
2020-01-03 01:24:36 -0300

    Update WideManager.php

--- src/WideManager.php
+++ src/WideManager.php
@@ -2,81 +2,79 @@
 
 class WideManager {
 
-	private $dominio = null;
-	private $token = null;
+    private $dominio;
+    private $token;
 
-	public $requisicoes = array();
+    public $requisicoes = array();
 
-	public function __construct($dominio, $token) {
+    public function __construct($dominio, $token = null) {
 
-		$this->dominio = $dominio;
-		$this->token = $token;
+        $this->dominio = $dominio;
+        $this->token = $token;
 
-	}
+    }
 
-	public function api($local, $parametros = array()) {
+    public function api($local, $parametros = array()) {
 
-		if (!$this->dominio) {
+        if (!$this->dominio) {
 
-			$requisicao = array(
-				'sucesso' => false,
-				'erro' => 'É necessário informar o domínio para efetuar a comunicação.'
-			);
+            $requisicao = array(
+                'sucesso' => false,
+                'erro' => 'É necessário informar o domínio para efetuar a comunicação.'
+            );
 
-		} else if (!$this->token) {
+        } else {
 
-			$requisicao = array(
-				'sucesso' => false,
-				'erro' => 'É necessário informar o token para efetuar a autenticação.'
-			);
+            $header = array(
+                'Accept: application/json'
+            );
+            
+            if ($this->token) {
+                $header[] = 'Authorization: Bearer ' . $this->token;
+            }
 
-		} else {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, 'http://' . $this->dominio . '/api/' . trim($local, '/'));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header); 
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parametros));
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+            $exec = curl_exec($curl);
+            curl_close($curl);
 
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, 'http://' . $this->dominio . '/api/' . trim($local, '/'));
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-				'Accept: application/json',
-				'Authorization: Bearer ' . $this->token
-			)); 
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parametros));
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-			$exec = curl_exec($curl);
-			curl_close($curl);
+            if ($exec) {
 
-			if ($exec) {
+                $requisicao = json_decode($exec, true);
 
-				$requisicao = json_decode($exec, true);
+                if (!is_array($requisicao)) {
 
-				if (!is_array($requisicao)) {
+                    $requisicao = array(
+                        'sucesso' => false,
+                        'erro' => 'Não foi possível tratar o retorno.',
+                    );
 
-					$requisicao = array(
-						'sucesso' => false,
-						'erro' => 'Não foi possível tratar o retorno.',
-					);
+                    if ($exec) {
+                        $requisicao['retorno'] = $exec;
+                    }
 
-					if ($exec) {
-						$requisicao['retorno'] = $exec;
-					}
+                }
 
-				}
+            } else {
 
-			} else {
+                $requisicao = array(
+                    'sucesso' => false,
+                    'erro' => 'Sem comunicação com o servidor.'
+                );
 
-				$requisicao = array(
-					'sucesso' => false,
-					'erro' => 'Sem comunicação com o servidor.'
-				);
+            }
 
-			}
+        }
 
-		}
+        $this->requisicoes[] = (object) $requisicao;
 
-		$this->requisicoes[] = (object) $requisicao;
+        return end($this->requisicoes);
 
-		return end($this->requisicoes);
-
-	}
+    }
 
 }
